7e812a77e6dd0af86c4e15a78b2bd157ae77791d
Pierce Issah <PJIceskull2003@gmail.com>
2023-09-14 11:16:30 -0400

    target p class through DOM

---
+++ Week_3/homework-two/callbackFunct.js
@@ -0,0 +1,6 @@
+// Variables
+// DOM
+let paragraphTags = document.getElementsByTagName("p");
+let numberPara = document.querySelector(".average");
+let totalValuePara = document.querySelector(".totalValue");
+let averagePara = document.querySelector(".average");
