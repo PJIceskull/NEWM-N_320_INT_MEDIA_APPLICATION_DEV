eec98a4a853458ef4d34bf44c0e688055cc5f461
Pierce Issah <PJIceskull2003@gmail.com>
2023-09-14 12:00:09 -0400

    created function to find math values

--- Week_3/homework-two/callbackFunct.js
+++ Week_3/homework-two/callbackFunct.js
@@ -4,3 +4,17 @@ let paragraphTags = document.getElementsByTagName("p");
 let numberPara = document.querySelector(".average");
 let totalValuePara = document.querySelector(".totalValue");
 let averagePara = document.querySelector(".average");
+
+// Higer order function
+// parameters: two numeric values, and a callback function
+function arithmeticFinder(x, y, newFn) {
+    // find the arithmetic values of addition, subtraction, multiplication and division of the two numbers
+    // Addition
+    let sumAddition = x + y;
+    // Subtraction
+    let sumSubtract = x - y;
+    // Multiplication
+    let sumProduct = x * y;
+    // Division
+    let sumQuotient = x / y;
+}
