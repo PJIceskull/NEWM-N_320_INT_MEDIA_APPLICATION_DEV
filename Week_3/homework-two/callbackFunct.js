6a9ae2dad685a832ff9eb469f9181f79373a132e
Pierce Issah <PJIceskull2003@gmail.com>
2023-09-14 13:15:33 -0400

    continue work on projects

--- Week_3/homework-two/callbackFunct.js
+++ Week_3/homework-two/callbackFunct.js
@@ -1,13 +1,25 @@
 // Variables
 // DOM
 let paragraphTags = document.getElementsByTagName("p");
-let numberPara = document.querySelector(".average");
+let numberPara = document.querySelector(".numbers");
 let totalValuePara = document.querySelector(".totalValue");
 let averagePara = document.querySelector(".average");
+// Numbers
+let num1 = 50;
+let num2 = 5;
+
+// Print to Console
+console.log(paragraphTags);
+// console.log(numberPara);
+// console.log(totalValuePara);
+// console.log(averagePara);
+
+// Write numbers to paragraph
+numberPara.innerHTML = "Numbers Chosen: " + num1 + " & " + num2;
 
 // Higer order function
 // parameters: two numeric values, and a callback function
-function arithmeticFinder(x, y, newFn) {
+function arithmeticFinder(x, y, callBackFn) {
     // find the arithmetic values of addition, subtraction, multiplication and division of the two numbers
     // Addition
     let sumAddition = x + y;
@@ -17,4 +29,16 @@ function arithmeticFinder(x, y, newFn) {
     let sumProduct = x * y;
     // Division
     let sumQuotient = x / y;
+    // Total Value
+    let totalValue = sumAddition + sumSubtract + sumProduct + sumQuotient;
+
+
+    console.log(sumAddition);
+    console.log(sumSubtract);
+    console.log(sumProduct);
+    console.log(sumQuotient);
+    console.log(totalValue);
 }
+
+// Call higer order function.
+arithmeticFinder(num1, num2);
