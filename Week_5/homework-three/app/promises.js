dcd75ac610cde187cfb539d7807ffeb4c77eb6a4
Pierce Issah <PJIceskull2003@gmail.com>
2023-09-26 13:31:59 -0400

    add async functions for promise

---
+++ Week_5/homework-three/app/promises.js
@@ -0,0 +1,52 @@
+// Fetch data from api
+let quoteFetch = fetch("https://api.quotable.io/quotes?limit=20");
+
+// Async function to fetch quote data
+async function getQoute() {
+  // API call
+  // const quoteFetch = await fetch("https://api.quotable.io/quotes?limit=20");
+  const quotes = await quoteFetch;
+
+  //Format into json
+  const results = await quotes.json();
+
+  return results;
+}
+
+// Function for display text in HTML files
+async function createPara(text) {
+  const paraTag = document.createElement("p");
+  paraTag.style.backgroundColor = "lightgray";
+  paraTag.style.borderRadius = "15px";
+  paraTag.style.padding = "10px";
+  // Insert Text using parameter & innerText
+  paraTag.innerText = text;
+  document.querySelector("body").appendChild(paraTag);
+}
+
+createPara("Apples");
+
+getQoute()
+  .then((data) => {
+    console.log(data);
+    // document.getElementsByTagName("body");
+    // Target body tag
+    bodyTag = document.querySelector("body");
+    // document.querySelector("body").innerText = data.results[0].author;
+
+    //   Use loop to print out values
+    // for (i = 0; i < 20; i++) {
+    //   console.log(data.results[i].author);
+    //   console.log(data.results[i].content);
+    // }
+
+    // data.results.forEach(function (value) {
+    //   console.log(value.author);
+    //   //   bodyTag.innerHTML += value.author;
+    //   console.log(value.content);
+    // });
+  })
+  .catch((error) => {
+    console.log("There was an error");
+    console.error();
+  });
