d2c9deb8415783b0314fa45dabae3ca4f282658e
Pierce Issah <PJIceskull2003@gmail.com>
2023-10-05 14:36:37 -0400

    add examples for classes.js file

--- Week_6/classes/classes.js
+++ Week_6/classes/classes.js
@@ -25,3 +25,45 @@ class Student extends Person {
 
 let Abigail = new Student("Abigail", 23, "Theater");
 Abigail.greeting();
+
+// Vehicle class
+class Vehicle {
+  constructor(make, model, year) {
+    this.make = make;
+    this.model = model;
+    this.year = year;
+  }
+
+  vInfo() {
+    return `This vehicle is a ${this.year} ${this.make} ${this.model}.`;
+  }
+}
+
+class Car extends Vehicle {
+  constructor(make, model, year, doors, transmission) {
+    super(make, model, year);
+    this.doors = doors;
+    this.transmission = transmission;
+    this.wheels = 4;
+  }
+}
+
+let charger = new Car("Dodge", "Charger", 2000, 4, "automatic");
+
+console.log(charger.vInfo());
+console.log(charger.doors, charger.transmission, charger.wheels);
+
+class Motorcycle extends Vehicle {
+  constructor() {
+    super(...arguments);
+    this.wheels = 2;
+  }
+  caution() {
+    return "Make sure you purchase a helmet.";
+  }
+}
+
+let hdCycle = new Motorcycle("Harley Davidson", "ES210", 2023);
+
+console.log(hdCycle.vInfo());
+console.log(hdCycle.caution());
