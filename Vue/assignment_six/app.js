301c0c1b7054028cc775815a5bffd1728f6b4895
Pierce Issah <PJIceskull2003@gmail.com>
2023-11-02 12:03:33 -0400

    display information

--- Vue/assignment_six/app.js
+++ Vue/assignment_six/app.js
@@ -2,17 +2,28 @@ const vueMount = Vue.createApp({
   data() {
     return {
       Name: "Pierce Issah", // Name (string)
-      Course: "", // Course (string)
-      AssignmentName: "", // Assignment Name (string)
-      Difficulty: 5, // Difficulty Level (integer)
+      Course: "N320", // Course (string)
+      AssignmentName: "First Vue Assignment", // Assignment Name (string)
+      Difficulty: 4, // Difficulty Level (integer)
 
       // Assignment Due Date (method call)
-
+      //   DueDate: new Date(),
       Completed: true, // Completed (boolean)
     };
   },
-  methods: {},
+  methods: {
+    // Assignment Due Date (method call)
+    DueDate() {
+      let newDate = new Date();
+      newDate.setDate(newDate.getDate() + 7); // Increase date by 7 days
+      newDate = newDate.toLocaleDateString();
+      return newDate;
+    },
+  },
 });
+
+// "My List" component
+app.component("My-List", {});
 
 // Target the article tag
 vueMount.mount("article");
