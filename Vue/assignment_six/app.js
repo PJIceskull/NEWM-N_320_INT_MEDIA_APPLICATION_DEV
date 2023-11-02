56561756be7f7e91a72bc151fe8c795ed89c0f22
Pierce Issah <PJIceskull2003@gmail.com>
2023-11-02 12:35:45 -0400

    create My list component

--- Vue/assignment_six/app.js
+++ Vue/assignment_six/app.js
@@ -5,7 +5,6 @@ const vueMount = Vue.createApp({
       Course: "N320", // Course (string)
       AssignmentName: "First Vue Assignment", // Assignment Name (string)
       Difficulty: 4, // Difficulty Level (integer)
-
       // Assignment Due Date (method call)
       //   DueDate: new Date(),
       Completed: true, // Completed (boolean)
@@ -23,7 +22,46 @@ const vueMount = Vue.createApp({
 });
 
 // "My List" component
-app.component("My-List", {});
+vueMount.component("My-List", {
+  template: `<ul>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>`,
+  data() {
+    // Create an array of 7 items or more
+    return {
+      birds: [
+        "Cardinal",
+        "Falcon",
+        "Penguin",
+        "Eagle",
+        "Vulture",
+        "Robin",
+        "Blue jay",
+        "Tucan",
+        "Crow",
+      ],
+    };
+  },
+  methods: {
+    displayItems(arr) {
+      // Target ul tag
+      let ulTag = document.getElementsByTagName("ul");
+      let liTags = document.getElementsByTagName("li");
+      arr.forEach(function (i) {
+        document.createElement("li"); // create li tag
+
+        liTags[i].innerText = "text";
+        liTags[i].appendChild();
+      });
+    },
+  },
+});
 
 // Target the article tag
 vueMount.mount("article");
